031e5f62af192953ad9b979745fabc8f9540a0f7
kafuivenu <kafuivenu@gmail.com>
2021-04-30 01:24:20 +0000

    migrate from truffle to hardhat, set up linters

--- test/token.js
+++ test/token.js
@@ -1,111 +1,107 @@
-const Token = artifacts.require('Token.sol');
-const truffleAssert = require('truffle-assertions');
+const { expect } = require('chai')
+const toWei = ethers.utils.parseEther
 
-contract('Token', accounts=> {
-  let token;
-  const [admin, holder] = accounts;
-  const TOTAL_SUPPLY = web3.utils.toWei('100000000');
+describe('Token contract', function () {
+  let token, admin, holder
+  const TOTAL_SUPPLY = toWei('100000000')
 
   before(async () => {
-    token = await Token.new();
-  });
+    [admin, holder] = await ethers.getSigners()
+
+    const Token = await ethers.getContractFactory('Token')
+    token = await Token.deploy()
+  })
 
   describe('balances tests', function () {
     it('should have total supply of 100,000,000', async () => {
-      const totalSupply = await token.totalSupply();
-      assert(totalSupply.toString() === TOTAL_SUPPLY);
-    });
+      expect((await token.totalSupply()).toString()).to.equal(TOTAL_SUPPLY)
+    })
 
     it('admin should have total supply', async () => {
-      const balanceAdmin = await token.balanceOf(admin);
-      assert(balanceAdmin.toString() === TOTAL_SUPPLY);
-    });
-  });
+      expect((await token.balanceOf(admin.address)).toString()).to.equal(TOTAL_SUPPLY)
+    })
+  })
 
   describe('pausing tests', function () {
     beforeEach(async function () {
-      const paused = await token.paused();
+      const paused = await token.paused()
 
       if (paused) {
-        await token.unpause({ from: admin });
+        await token.unpause({ from: admin.address })
       }
-    });
+    })
 
     it('admin can pause', async function () {
-      const { logs } = await token.pause({ from: admin });
+      await expect(token.pause({ from: admin.address }))
+        .to.emit(token, 'Paused')
 
-      assert.equal(logs.length, 1);
-      assert.equal(logs[0].event, 'Paused');
-
-      assert.equal(await token.paused(), true);
-    });
+      expect(await token.paused()).to.equal(true)
+    })
 
     it('admin can unpause', async function () {
-      await token.pause({ from: admin });
-      const { logs } = await token.unpause({ from: admin });
+      await token.pause({ from: admin.address })
+      await expect(token.unpause({ from: admin.address }))
+        .to.emit(token, 'Unpaused')
 
-      assert.equal(logs.length, 1);
-      assert.equal(logs[0].event, 'Unpaused');
-
-      assert.equal(await token.paused(), false);
-    });
+      await expect(await token.paused()).to.equal(false)
+    })
 
     it('only admin can pause', async function () {
-      await truffleAssert.reverts(token.pause({ from: holder }));
-    });
+      await expect(token.pause({ from: holder })).to.be.reverted
+    })
 
     it('only admin can unpause', async function () {
-      await token.pause({ from: admin });
-      assert.equal(await token.paused(), true);
+      await token.pause({ from: admin.address })
+      await expect(await token.paused()).to.equal(true)
 
-      await truffleAssert.reverts(token.unpause({ from: holder }));
-    });
+      await expect(token.unpause({ from: holder })).to.be.reverted
+    })
 
     it('transfers happen when not paused', async function () {
-      const amountToTransfer = 1000;
-      const initialHolderBalance = (await token.balanceOf(holder)).toNumber();
-      await token.transfer(holder, amountToTransfer, {from: admin});
+      const amountToTransfer = 1000
+      const initialHolderBalance = (await token.balanceOf(holder.address)).toNumber()
+      await token.transfer(holder.address, amountToTransfer, { from: admin.address })
 
-      const expectedTotalHolderBalance = initialHolderBalance + amountToTransfer;
-      const currentBalance = (await token.balanceOf(holder)).toNumber();
+      const expectedTotalHolderBalance = initialHolderBalance + amountToTransfer
+      const currentBalance = (await token.balanceOf(holder.address)).toNumber()
 
-      assert(currentBalance === expectedTotalHolderBalance, true);
-    });
+      expect(currentBalance).to.be.equal(expectedTotalHolderBalance)
+    })
 
     it('no transfers when paused', async function () {
-      await token.pause({ from: admin });
-      await truffleAssert.reverts(token.transfer(holder, 100, {from: admin}));
-    });
+      await token.pause({ from: admin.address })
+      await expect(token.transfer(holder.address, 100, { from: admin.address })).to.be.reverted
+    })
 
     it('transfers work after pausing and unpausing', async function () {
-      await token.pause({ from: admin });
-      await token.unpause({ from: admin });
+      await token.pause({ from: admin.address })
+      await token.unpause({ from: admin.address })
 
-      const amountToTransfer = 1000;
-      const initialHolderBalance = (await token.balanceOf(holder)).toNumber();
+      const amountToTransfer = 1000
+      const initialHolderBalance = (await token.balanceOf(holder.address)).toNumber()
 
-      await token.transfer(holder, amountToTransfer, {from: admin});
+      await token.transfer(holder.address, amountToTransfer, { from: admin.address })
 
-      const expectedTotalHolderBalance = initialHolderBalance + amountToTransfer;
-      const currentBalance = (await token.balanceOf(holder)).toNumber();
+      const expectedTotalHolderBalance = initialHolderBalance + amountToTransfer
+      const currentBalance = (await token.balanceOf(holder.address)).toNumber()
 
-      assert(currentBalance === expectedTotalHolderBalance, true);
-    });
-  });
+      expect(currentBalance).to.be.equal(expectedTotalHolderBalance)
+    })
+  })
 
   describe('burn tests', function () {
     it('holders can burn', async function () {
-      const amountToTransfer = 1000;
-      const amountToBurn = 500;
-      const initialHolderBalance = (await token.balanceOf(holder)).toNumber();
-      await token.transfer(holder, amountToTransfer, {from: admin});
-      await token.burn(amountToBurn, {from: holder});
+      const amountToTransfer = 1000
+      const amountToBurn = 500
+      const initialHolderBalance = (await token.balanceOf(holder.address)).toNumber()
 
-      const expectedTotalHolderBalance = initialHolderBalance + amountToTransfer - amountToBurn;
-      const currentBalance = (await token.balanceOf(holder)).toNumber();
+      await token.transfer(holder.address, amountToTransfer)
+      await token.connect(holder).burn(amountToBurn)
 
-      assert(currentBalance === expectedTotalHolderBalance, true);
-    });
-    
-  });
-});
+      const expectedTotalHolderBalance = initialHolderBalance + amountToTransfer - amountToBurn
+      const currentBalance = (await token.balanceOf(holder.address)).toNumber()
+
+      expect(currentBalance).to.be.equal(expectedTotalHolderBalance)
+    })
+  })
+})
